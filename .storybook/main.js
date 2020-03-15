dc3be1d1bbe6271d4a0506ac2455d707ae629cd8
NoName4Me <anich.evas@gmail.com>
2020-03-15 18:24:42 +0800

    chore: storybook config done.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,38 +1,10 @@
-const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.(mdx|tsx)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-storysource',
-    {
-      name: '@storybook/addon-docs',
-      // options: {
-      //   configureJSX: true,
-      //   babelOptions: {},
-      //   sourceLoaderOptions: null,
-      // },
-    },
+    '@storybook/addon-docs',
     '@storybook/preset-typescript',
+    '@storybook/preset-scss',
   ],
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-        // Optional
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-      ],
-    });
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
 };
